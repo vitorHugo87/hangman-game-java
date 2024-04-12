4576968d0c809cba65bf99560a2785aeab241ce4
vitorHugo87 <vitorpaduan87@gmail.com>
2024-04-11 21:51:58 -0300

    Showing progress

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,7 +11,21 @@ public class Program {
 		wordMethods.initialSetup();
 		Word result = wordMethods.drawnWord(wordMethods.words);
 		
-		System.out.println(result.getWord());
+		String[] progress = new String[result.getWord().length()];
+		for(int i = 0; i < progress.length; i++) {
+			if(result.getWord().charAt(i) != ' ') {
+				progress[i] = "_";
+			}
+			else {
+				progress[i] = " ";
+			}
+		}
+		
 		System.out.println(result.getTheme());
+		System.out.println(result.getWord());
+		for(int i = 0; i < progress.length; i++) {
+			System.out.print(progress[i] + " ");
+		}
+		
 	}
 }
